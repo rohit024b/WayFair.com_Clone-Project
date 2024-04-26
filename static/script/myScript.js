d642e262b91b2791f5a4e7327778ab84e558e2e7
Rohit Bhise <35328235+rohit024b@users.noreply.github.com>
2024-04-26 23:24:46 +0530

    Update index.html
    - New scroll to top button added with complete functionality and one chat bot button.

--- static/script/myScript.js
+++ static/script/myScript.js
@@ -3,14 +3,18 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110) {
         document.getElementById("StillnavBar").style.position = 'fixed'
-        document.getElementById("StillnavBar").style.top = "0";
+        document.getElementById("StillnavBar").style.top = "0px";
         document.getElementById("StillnavBar2").style.position = 'fixed'
         document.getElementById("StillnavBar2").style.top = "0";
+        document.getElementById("gToTop").style.position = 'fixed'
+        document.getElementById("gToTop").style.right= "30px";
         
     } else {
         document.getElementById("StillnavBar").style.position = ''
         document.getElementById("StillnavBar").style.top = "-80px";
         document.getElementById("StillnavBar2").style.position = ''
         document.getElementById("StillnavBar2").style.top = "-80px";
+        document.getElementById("gToTop").style.position = ''
+        document.getElementById("gToTop").style.right= "-100px";
     }
 }
